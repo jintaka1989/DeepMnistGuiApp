287e7548f7f6333c8377f8b60a3d50753a958602
takajin1989 <ya65857@ubuntu.ubuntu-domain>
2016-10-19 18:17:26 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+TensorFlowのチュートリアル(Deep MNIST for Experts)を自分の書いた数字で試すGUIアプリケーション
+
+開発環境(Ubuntu)：
+OS:Ubuntu 16.04 LTS 64bit
+CPU:Core i7-5500U @ 2.40GHz × 4
+GPU:標準Intel HD Graphics 5500
+メモリ:8GB
+言語:Python 2.7.12
+
+sudo apt-get install libopencv-dev
+sudo apt-get install python-opencv
+sudo apt-get install python-scipy
+sudo apt-get install python-matplotlib
+sudo apt-get install python-numpy
+sudo apt-get install python-qt4
+
+
+
+Copyright
+
+see ./LICENSE
+Copyright 2016 Satoshi TAKAGI <jintaka198912@gmail.com>
--- mnist_recognizer.py
+++ mnist_recognizer.py
@@ -1,3 +1,17 @@
+   # Copyright 2016 Satoshi TAKAGI <jintaka198912@gmail.com>
+   #
+   # Licensed under the Apache License, Version 2.0 (the "License");
+   # you may not use this file except in compliance with the License.
+   # You may obtain a copy of the License at
+   #
+   #     http://www.apache.org/licenses/LICENSE-2.0
+   #
+   # Unless required by applicable law or agreed to in writing, software
+   # distributed under the License is distributed on an "AS IS" BASIS,
+   # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   # See the License for the specific language governing permissions and
+   # limitations under the License.
+
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -77,7 +91,7 @@ def mnist_recognizer(image):
 
 	answer = tf.argmax(y_conv,1)
 	result = sess.run(answer, feed_dict={x: image, keep_prob: 1.0})
-	
+
 	return result
 
 def close():
