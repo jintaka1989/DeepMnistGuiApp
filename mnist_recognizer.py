193d0c159d73b495901a40917c6de1e1bd97b26f
Jintaka <s.takagiab@gmail.com>
2016-12-05 21:08:06 +0900

    Update mnist_recognizer.py

--- mnist_recognizer.py
+++ mnist_recognizer.py
@@ -12,8 +12,8 @@
    # See the License for the specific language governing permissions and
    # limitations under the License.
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 import tensorflow as tf
 from PIL import Image
